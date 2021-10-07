c95eb43f06fa5e4ee1befe87c5dd36e1f30607f4
omidashouri <omidashouri@gmail.com>
2021-10-07 23:32:13 +0330

    3.6. POSTing Data via HTML Form to Java : omidashouri : 000715

--- src/main/java/ir/omidashouri/marsroverapi/controllers/HomeController.java
+++ src/main/java/ir/omidashouri/marsroverapi/controllers/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,17 +16,35 @@ public class HomeController {
 
     private final MarsRoverApiService marsRoverApiService;
 
-    @GetMapping("/")
-    public String getHomeView(ModelMap modelMap){
+    @GetMapping(value = {"/"})
+    public String getHomeView(ModelMap modelMap) {
 //        curiosity opportunity
         MarsRoverApiResponse marsRoverApiResponse = marsRoverApiService.getRoverData("opportunity");
         modelMap.put("marsRoverData", marsRoverApiResponse);
         return "index";
     }
 
+/*
+    @GetMapping(value = {"/redirect"})
+    public String redirectPostResponseToHomeView(ModelMap modelMap){
+        MarsRoverApiResponse marsRoverApiResponse = marsRoverApiService.getRoverData("opportunity");
+        modelMap.put("marsRoverData", marsRoverApiResponse);
+        return "index";
+    }
+*/
+
+    //    when redirect data is not saved in the page
+    @PostMapping("/")
+    public String getHomeView(ModelMap modelMap, @RequestParam String marsApiRoverData) {
+        MarsRoverApiResponse marsRoverApiResponse = marsRoverApiService.getRoverData(marsApiRoverData);
+        modelMap.put("marsRoverData", marsRoverApiResponse);
+//        return "index";
+        return "redirect:/";
+    }
+
 
     @GetMapping("/1")
-    public String getHomeView1(Model model){
+    public String getHomeView1(Model model) {
         //we can use 'ModelMap' instead 'Model' and use modelMap.put("name", "omidashouri")
         model.addAttribute("name", "omidashouri");
         return "index1";
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,17 @@
+/*
 document.getElementById('curiosity').addEventListener('click', curiosityFunction);
 
 function curiosityFunction() {
     alert('salam');
+}*/
+
+let marsApiButtons = document.querySelectorAll("button[id*='marsApi']");
+marsApiButtons.forEach(button => button.addEventListener('click', buttonFunction));
+
+function buttonFunction() {
+    const buttonId = this.id;
+    const roverId = buttonId.split('Api')[1];
+    let apiData = document.getElementById('marsApiRoverData');
+    apiData.value = roverId;
+    document.getElementById('frmRoverType').submit();
 }
