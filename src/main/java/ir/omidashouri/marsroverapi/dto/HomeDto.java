d8c0f83ca019793b621d56d63516329a9e767214
omidashouri <omidashouri@gmail.com>
2021-10-09 09:24:03 +0330

    2.12. Adding Mars Cameras to the View -> finished : omidashouri : 000717

--- src/main/java/ir/omidashouri/marsroverapi/dto/HomeDto.java
+++ src/main/java/ir/omidashouri/marsroverapi/dto/HomeDto.java
@@ -7,6 +7,14 @@ public class HomeDto {
 
     private String marsApiRoverData;
     private Integer marsSol;
-    private Boolean defaultCheck1;
+    private Boolean cameraFhaz;
+    private Boolean cameraRhaz;
+    private Boolean cameraMast;
+    private Boolean cameraChemcam;
+    private Boolean cameraMahli;
+    private Boolean cameraMardi;
+    private Boolean cameraNavcam;
+    private Boolean cameraPancam;
+    private Boolean cameraMinites;
 
 }
